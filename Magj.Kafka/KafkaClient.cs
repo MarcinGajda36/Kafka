5293b22e573e6e18db6a6eb2236ec9ff82acfc14
Marcin Gajda <kirgo36@gmail.com>
2025-10-27 20:13:02 +0100

    Documenting settings

--- Magj.Kafka/KafkaClient.cs
+++ Magj.Kafka/KafkaClient.cs
@@ -14,11 +14,43 @@ public sealed partial class KafkaClient
         string BootstrapServers,
         string GroupId)
     {
+        /// <summary>
+        /// Number of times processor function can be called in parallel.
+        /// Only change above 1 it if processing multiple messages at the same time is safe.
+        /// 1 by default. -1 for unbounded. 
+        /// </summary>
         public int MaxDegreeOfParallelism { get; init; } = 1;
+
+        /// <summary>
+        /// Ammount of messages that can wait in memory for processing.
+        /// Can be increased for potential perf improvements, or decreased to consume less memory.
+        /// 4096 by default. -1 for unbounded.
+        /// </summary>
         public int MaxBufferedMessages { get; init; } = 4096;
+
+        /// <summary>
+        /// How long we wait for message before we loop and try again.
+        /// </summary>
         public TimeSpan ConsumeTimeout { get; init; } = TimeSpan.FromSeconds(1);
+
+        /// <summary>
+        /// Scheduler used for recieving kafka messages and storing their offset after processing. 
+        /// Leaving default is recommended.
+        /// TaskScheduler.Default is default.
+        /// </summary>
         public TaskScheduler ConsumerScheduler { get; init; } = TaskScheduler.Default;
+
+        /// <summary>
+        /// Scheduler used for processing kafka messages.
+        /// TaskScheduler.Default is default.
+        /// </summary>
         public TaskScheduler ProcessorScheduler { get; init; } = TaskScheduler.Default;
+
+        /// <summary>
+        /// Logger used to log Exceptions.
+        /// Fatal kafka exception and Unhandled processor exceptions are still causing throw, so i may not be necesary to asign this.
+        /// NullLogger.Instance by default.
+        /// </summary>
         public ILogger Logger { get; init; } = NullLogger.Instance;
     }
 
